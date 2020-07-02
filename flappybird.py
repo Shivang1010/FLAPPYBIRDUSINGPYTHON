b603b4fa8d953587a4e721d2c6dd24bd81185c26
Shivang Srivastava <55347605+Shivang1010@users.noreply.github.com>
2020-07-02 16:29:34 +0530

    Create flappybird.py

---
+++ flappybird.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+"""Flappy Bird, implemented using Pygame."""
+
+import math
+import os
+from random import randint
+from collections import deque
+
+import pygame
+from pygame.locals import *
