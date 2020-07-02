460e96a33a40a8b4c331bd4aa297e36dbfc05bd2
Shivang Srivastava <55347605+Shivang1010@users.noreply.github.com>
2020-07-02 16:32:15 +0530

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -292,3 +292,13 @@ def frames_to_msec(frames, fps=FPS):
     return 1000.0 * frames / fps
 
 
+def msec_to_frames(milliseconds, fps=FPS):
+    """Convert milliseconds to frames at the specified framerate.
+
+    Arguments:
+    milliseconds: How many milliseconds to convert to frames.
+    fps: The framerate to use for conversion.  Default: FPS.
+    """
+    return fps * milliseconds / 1000.0
+
+
